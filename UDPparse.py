a927dee22d36df0e66ab401ae32afcb704bf7318
Siamoza <siamoza@gmail.com>
2022-12-05 10:54:29 +0300

    Initial commit

---
+++ UDPparse.py
@@ -0,0 +1,10 @@
+# Установка диагностики подшипников УДП. База данных в формате DBISAM
+
+import os
+from datetime import datetime
+
+SOURCE = '/opt/datasets/udp/'
+DATASET = '/opt/datasets/udp/dataset.txt'
+
+if __name__ == '__main__':
+    udp = []
