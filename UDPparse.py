33c95b4e21de1f76b4f39462a897b59f9d66fbeb
Siamoza <siamoza@gmail.com>
2022-12-05 16:40:51 +0300

    0.0.1
    UDPparse.py: найдено решение и создан файл.

--- UDPparse.py
+++ UDPparse.py
@@ -1,6 +1,10 @@
-# Установка диагностики подшипников УДП. База данных в формате DBISAM
+# Установка диагностики подшипников УДП. База данных в формате DBISAM. Адекватного коннектора на Python под Linux
+# не найдено, поэтому применён комбинированный подход.
+# Из Windows с помощью программы Database System Utility все таблицы DAT были выгружены в TXT-формат.
+# Данный скрипт фильтрует содержимое этих файлов.
 
 import os
+import sys
 from datetime import datetime
 
 SOURCE = '/opt/datasets/udp/'
@@ -8,3 +12,7 @@ DATASET = '/opt/datasets/udp/dataset.txt'
 
 if __name__ == '__main__':
     udp = []
+    dir_list = [x[0] for x in os.walk(SOURCE)]  # список каталогов
+    dir_list.pop(0)  # лишний элемент, имя родительского каталога
+    for i in dir_list:
+
